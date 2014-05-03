282236704866cbf26cbde662c104e6fe981fff89
Alex Henning <elcron@gmail.com>
2014-05-02 21:22:01 -0400

    Added support for going to subdirectories of tabs.

--- file.go
+++ file.go
@@ -37,19 +37,38 @@ func NewFileSource(file string) source {
 }
 
 func (s *FileSource) Goto(str string) (string, error) {
-	dir, ok := s.shortcuts[str]
-	if !ok {
+	val, err := s.expand(str)
+	if err != nil {
 		return "", errors.New(fmt.Sprintf("No such shortcut %s", str))
 	}
-	return dir, nil
+	return val, err
 }
 
 func (s *FileSource) Complete(prefix string) []string {
 	var completions []string
 	for key := range s.shortcuts {
 		if strings.HasPrefix(key, prefix) {
-			completions = append(completions, key)
+			completions = append(completions, key+string(os.PathSeparator))
+		} else if strings.HasPrefix(prefix, key) {
+			val, err := s.expand(prefix)
+			n := strings.LastIndex(val, string(os.PathSeparator))
+			m := strings.LastIndex(prefix, string(os.PathSeparator))
+			if err != nil || n == -1 || m == -1 {
+				continue
+			}
+			for _, completion := range completionsForDir(val[:n], val[n+1:]) {
+				completions = append(completions, prefix[:m+1]+completion)
+			}
 		}
 	}
 	return completions
+}
+
+func (s *FileSource) expand(str string) (string, error) {
+	for key := range s.shortcuts {
+		if strings.HasPrefix(str, key) {
+			return s.shortcuts[key] + string(os.PathSeparator) + strings.TrimPrefix(str, key), nil
+		}
+	}
+	return "", errors.New(fmt.Sprintf("can't", str))
 }
